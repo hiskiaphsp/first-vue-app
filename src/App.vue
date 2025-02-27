20b250364b2c8254fbcc3705286f931499209c95
WDTI0\hiskia.parhusip <hiskia.parhusip@primecash.co.id>
2025-02-27 15:39:29 +0700

    mod:try to make component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen bg-gray-80g0">
+    <Navbar :navigation/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+import Navbar from "./components/Navbar.vue";
+const navigation = [
+  { name: 'Dashboard', href: '#', current: true },
+  { name: 'Team', href: '#', current: false },
+  { name: 'Projects', href: '#', current: false },
+  { name: 'Calendar', href: '#', current: false },
+]
+</script>
---
+++ src/components/forms/InputText.vue
@@ -0,0 +1,48 @@
+<template>
+  <input
+      type="text"
+      :id="id"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      class="shadow-xs border h-9 text-sm rounded-lg block w-full p-2.5 bg-gray-850 border-gray-700 placeholder-gray-400 text-white focus:ring-2 focus:ring-gray-600 focus:ring-offset-2 focus:ring-offset-gray-600 focus:outline-hidden shadow-xs-light"
+      :class="{ 'border-red-500': error }"
+      :name="name"
+  />
+</template>
+<script>
+export default {
+  name: "InputText",
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    type: {
+      type: String,
+      default: "text", // Jenis input (text, email, password, dll.)
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+    modelValue: {
+      type: [String, Number],
+      default: "",
+    },
+    error: {
+      type: String,
+      default: "",
+    },
+  },
+  emits: ["update:modelValue"],
+};
+</script>
